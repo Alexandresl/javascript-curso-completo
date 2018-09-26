d4c8ce7050e1f101665ccde866b1f1ced7db979e
Alexandre <alexandre@alsites.com.br>
2018-09-26 20:08:03 -0300

    26/09/2018

--- curso-javascript-projeto-usuarios-old/index.js
+++
@@ -1,4 +0,0 @@
-/**
- * Criamos uma instância da classe UserController
- */
-let userController = new UserController("form-user-create", "form-user-update", "table-user");
---
+++ projeto-node-client-server/client-server/public/models/User.js
@@ -0,0 +1,249 @@
+/**
+ * Classe User
+ */
+class User {
+
+    /**
+     * Método construtor
+     */
+    constructor(name, gender, birth, country, email, password, photo, admin){
+
+        this._id;
+        this._name = name;
+        this._gender = gender;
+        this._birth = birth;
+        this._country = country;
+        this._email = email;
+        this._password = password;
+        this._photo = photo;
+        this._admin = admin;
+        this._register = new Date();
+
+    }
+
+    /**
+     * Métodos getters and setters
+     */
+    set id(value) {
+
+        this._id = value;
+
+    }
+
+    get id() {
+
+        return this._id;
+
+     }
+
+    set name(value) {
+
+        this._name = value;
+
+    }
+
+    get name() {
+
+        return this._name;
+
+    }
+
+    set gender(value) {
+
+        this._gender = value;
+
+    }
+
+    get gender() {
+
+        return this._gender;
+
+    }
+
+    set birth(value) {
+
+        this._birth = value;
+
+    }
+
+    get birth() {
+
+        return this._birth;
+
+    }
+
+    set country(value) {
+
+        this._country = value;
+
+    }
+
+    get country() {
+
+        return this._country;
+
+    }
+
+    set email(value) {
+
+        this._email = value;
+
+    }
+
+    get email() {
+
+        return this._email;
+
+    }
+
+    set password(value) {
+
+        this._password = value;
+
+    }
+
+    get password() {
+
+        return this._password;
+
+    }
+
+    set photo(value) {
+
+        this._photo = value;
+
+    }
+
+    get photo() {
+
+        return this._photo;
+
+    }
+
+    set admin(value) {
+
+        this._admin = value;
+
+    }
+
+    get admin() {
+
+        return this._admin;
+
+    }
+
+    get register() {
+
+        return this._register;
+
+    }
+
+    /**
+     * Método que recebe um JSON
+     * e carrega as informações
+     * nos atributos do objeto
+     */
+    loadFromJSON(json) {
+
+        /**
+         * for in para percorrer todos
+         * as chaves do json
+         */
+        for (let name in json) {
+
+            /**
+             * Tratamos para o caso de
+             * ser uma data.
+             */
+            switch(name) {
+
+                case '_register':
+                    this[name] = new Date(json[name]);
+                    break;
+                default:
+
+                    if (name.substring(0, 1) === '_') this[name] = json[name];
+                    break;
+            }
+
+        }
+
+    }
+
+    /**
+     * Pega os dados do session storage
+     * / local storage
+     */
+    static getUsersStorage() {
+
+        return Fetch.get('/users');
+
+    }
+
+    /**
+     * retorna o objeto em formato JSON
+     */
+    toJSON() {
+
+        let json = {};
+        
+        Object.keys(this).forEach(key => {
+
+            if (this[key] !== undefined) {
+
+                json[key] = this[key];
+
+            }
+
+        });
+
+        return json;
+
+    }
+
+    /**
+     * Método para salvar o usuário no Local Storage
+     */
+    save() {
+
+        return new Promise((resolve, reject) => {
+
+            let promise;
+
+            if (this.id) {
+
+                promise = Fetch.put(`/users/${this.id}`, this.toJSON());
+
+            } else {
+
+                promise = Fetch.post(`/users`, this.toJSON());
+
+            }
+
+            promise.then(data => {
+
+                this.loadFromJSON(data);
+
+                resolve(this)
+
+            }).catch(e => {
+                 
+                reject(e);
+
+            });
+
+        });        
+        
+    }
+
+    /**
+     * Irá remover o objeto do local storage
+     */
+    remove() {
+
+        return Fetch.delete(`/users/${this.id}`);
+
+    }
+
+
+}
