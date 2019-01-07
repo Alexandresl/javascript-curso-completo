6fd51c46149f6e911ed01c6dc1ab635f29b793f1
Alexandre <alexandre@alsites.com.br>
2019-01-07 21:47:42 -0200

    070120192147

--- Calc6/scripts/controller/CalcController.js
+++ Calc6/scripts/controller/CalcController.js
@@ -1,5 +1,34 @@
 class CalcController {
 
-    
+    constructor() {
+
+        this._displayCalc = '';
+        this._currentDate = '';
+
+    }
+
+    get displayCalc() {
+
+        return this.displayCalc;
+
+    }
+
+    set displayCalc(value) {
+
+        this._displayCalc = value;
+
+    }
+
+    get currentDate() {
+
+        return this._currentDate;
+
+    }
+
+    set currentDate(value) {
+
+        this._currentDate = value;
+
+    }
 
 }
