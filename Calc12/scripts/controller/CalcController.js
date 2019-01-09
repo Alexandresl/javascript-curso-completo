7dcab290b131df001214da1e2614ba0d645a98e2
Alexandre <alexandre@alsites.com.br>
2019-01-09 00:50:39 -0200

    090120190050

--- Calc12/scripts/controller/CalcController.js
+++ Calc12/scripts/controller/CalcController.js
@@ -2,20 +2,73 @@ class CalcController {
 
     constructor() {
 
-        this._displayCalc = '';
+        this._displayCalcEl = document.querySelector('#display-main');
+        this._dateEl = document.querySelector('.display-date');
+        this._timeEl = document.querySelector('.display-time');
+        this._historicEl = document.querySelector('#display-historic');
+        this._audioEl = document.querySelector('.display-audio');
         this._currentDate = '';
+        this.initialize();
 
     }
 
-    get displayCalc() {
+    get displayCalcEl() {
 
-        return this._displayCalc;
+        return this._displayCalcEl.innerHTML;
 
     }
 
-    set displayCalc(value) {
+    set displayCalcEl(value) {
 
-        this._displayCalc = value;
+        this._displayCalcEl.innerHTML = value;
+
+    }
+
+    get dateEl() {
+
+        return this._dateEl.innerHTML;
+
+    }
+
+    set dateEl(value) {
+
+        this._dateEl.innerHTML = value;
+
+    }
+
+    get timeEl() {
+
+        return this._timeEl.innerHTML;
+
+    }
+
+    set timeEl(value) {
+
+        this._timeEl.innerHTML = value;
+
+    }
+
+    get historicEl() {
+
+        return this._historicEl.innerHTML;
+
+    }
+
+    set historicEl(value) {
+
+        this._historicEl.innerHTML = value;
+
+    }
+
+    get audioEl() {
+
+        return this._audioEl.innerHTML;
+
+    }
+
+    set audioEl(value) {
+
+        this._audioEl.innerHTML = value;
 
     }
 
@@ -28,6 +81,16 @@ class CalcController {
     set currentDate(value) {
 
         this._currentDate = value;
+
+    }
+
+    initialize() {
+
+        this.displayCalcEl = 0;
+        this.dateEl = '09/01/2019';
+        this.timeEl = '00:47';
+        this.historicEl = '1 + 1';
+        this.audioEl = '♪';
 
     }
 
