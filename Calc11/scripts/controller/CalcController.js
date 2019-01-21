0bde9b5db921138d6ac05185413dc3d655c56df3
Alexandre <alexandre@alsites.com.br>
2019-01-21 00:04:07 -0200

    210120190004

--- Calc11/scripts/controller/CalcController.js
+++ Calc11/scripts/controller/CalcController.js
@@ -139,9 +139,25 @@ class CalcController {
 
     }
 
+    calc() {
+
+        let last = this._operation.pop();
+
+        let result = eval(this._operation.join(''));
+
+        this._operation = [result, last];
+
+    }
+
     pushOperation(value) {
 
         this._operation.push(value);
+
+        if (this._operation.length > 3) {
+
+            this.calc();
+
+        }
 
     }
 
@@ -179,9 +195,17 @@ class CalcController {
 
             // Number
 
-            let newValue = this.getLastOperation().toString() + value.toString();
+            if (this.isOperator(value)) {
 
-            this.setLastOperation(parseInt(newValue));
+                this.pushOperation(value);
+
+            } else {
+
+                let newValue = this.getLastOperation().toString() + value.toString();
+
+                this.setLastOperation(parseInt(newValue));
+
+            }
 
         }
 
