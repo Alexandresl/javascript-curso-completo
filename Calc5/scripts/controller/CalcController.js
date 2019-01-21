c22a926c39d3fb5d750adfcd6683350b4cdd6923
Alexandre <alexandre@alsites.com.br>
2019-01-20 23:29:13 -0200

    200120192329

--- Calc5/scripts/controller/CalcController.js
+++ Calc5/scripts/controller/CalcController.js
@@ -127,9 +127,63 @@ class CalcController {
 
     }
 
-    addOperation(value) {
+    getLastOperation() {
+
+        return this._operation[this._operation.length - 1];
+
+    }
+
+    setLastOperation(value) {
+
+        this._operation[this._operation.length - 1] = value;
+
+    }
+
+    pushOperation(value) {
 
         this._operation.push(value);
+
+    }
+
+    isOperator(value) {
+
+        return (['+', '-', '*', '/', '%'].indexOf(value) > -1)
+
+    }
+
+    addOperation(value) {
+
+        if (isNaN(this.getLastOperation())) {
+
+            // String
+
+            if (this.isOperator(value)) {
+
+                // Change operator
+
+                this.setLastOperation(value);
+
+            } else if (isNaN(value)) {
+
+                // Dot
+
+            } else {
+
+                // First Number
+
+                this.pushOperation(value);
+
+            }
+
+        } else {
+
+            // Number
+
+            let newValue = this.getLastOperation().toString() + value.toString();
+
+            this.setLastOperation(parseInt(newValue));
+
+        }
 
         console.log(this._operation);
 
@@ -148,23 +202,23 @@ class CalcController {
                 break;
 
             case 'soma':
-
+                this.addOperation('+');
                 break;
 
             case 'subtracao':
-
+                this.addOperation('-');
                 break;
 
             case 'multiplicacao':
-
+                this.addOperation('*');
                 break;
 
             case 'divisao':
-
+                this.addOperation('/');
                 break;
 
             case 'porcento':
-
+                this.addOperation('%');
                 break;
 
             case 'igual':
@@ -172,7 +226,7 @@ class CalcController {
                 break;
 
             case 'ponto':
-
+                this.addOperation('.');
                 break;
 
             case '0':
