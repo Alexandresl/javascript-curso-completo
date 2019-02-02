4a7d2875fa3a1d12f8722c8ebf0f9dffb0010cae
Alexandre Lima <limaalexguitar@gmail.com>
2019-02-02 03:27:31 -0200

    020220190327

---
+++ Calc1/scripts/CalcController/CalcController.js
@@ -0,0 +1,93 @@
+class CalcController {
+
+    constructor() {
+
+        this._displayCalcEl = document.querySelector('#display-main');
+        this._timeEl = document.querySelector('.display-time');
+        this._dateEl = document.querySelector('.display-date');
+        this._historicEl = document.querySelector('#display-historic');
+        this._audioEl = document.querySelector('.display-audio');
+        this._currentDate = '';
+        this.initialize();
+
+    }
+
+    get displayCalcEl() {
+
+        return this._displayCalcEl.innerHTML;
+
+    }
+
+    set displayCalcEl(value) {
+
+        this._displayCalcEl.innerHTML = value;
+
+    }
+
+    get timeEl() {
+
+        return this._timeEl.innerHTML;
+
+    }
+
+    set timeEl(value) {
+
+        this._timeEl.innerHTML = value;
+
+    }
+
+    get dateEl() {
+
+        return this._dateEl.innerHTML;
+
+    }
+
+    set dateEl(value) {
+
+        this._dateEl.innerHTML = value;
+
+    }
+
+    get historicEl() {
+
+        return this._historicEl.innerHTML;
+
+    }
+
+    set historicEl(value) {
+
+        this._historicEl.innerHTML = value;
+
+    }
+
+    get audioEl() {
+
+        return this._audioEl.innerHTML;
+
+    }
+
+    set audioEl(value) {
+
+        this._audioEl.innerHTML = value;
+
+    }
+
+    get currentDate() {
+
+        return this._currentDate;
+
+    }
+
+    set currentDate(value) {
+
+        this._currentDate = value;
+
+    }
+
+    initialize() {
+
+        this.displayCalcEl = 0;
+
+    }
+
+}
